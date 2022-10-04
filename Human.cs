1ad59ff710853cca9b20b30e34948bdda1638e2e
cgreg4000 <calebggregory@gmail.com>
2022-10-04 08:41:19 -0500

    first commit

---
+++ Human.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Human
+{
+    public class Human
+    {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+        public int Health { get { return health; } }
+
+        public Human(string n)
+        {
+            Name = n;
+            Strength = 3;
+            Intelligence = 3;
+            Dexterity = 3;
+            health = 100;
+        }
+        public Human(string n, int s, int i, int d, int h)
+        {
+            Name = n;
+            Strength = s;
+            Intelligence = i;
+            Dexterity = d;
+            health = h;
+        }
+
+        public int Attack(Human target)
+        {
+            target.health -= 5 * this.Strength;
+            return target.health;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human h1 = new Human("Oreo");
+            Console.WriteLine(h1.Name);
+            Console.WriteLine(h1.Strength);
+
+            Human h2 = new Human("Gandalf", 5, 10, 2, 110);
+            Console.WriteLine(h2.Name);
+            Console.WriteLine(h2.Strength);
+
+            h1.Attack(h2);
+            Console.WriteLine(h2.Health);
+        }
+    }
+}
